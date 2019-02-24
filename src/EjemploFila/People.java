354bd2a081189c251285218053150034f1bcd3c1
Nicolas Salazar <njse22@gmail.com>
2019-02-23 20:23:01 -0500

    translate classes

---
+++ src/EjemploFila/People.java
@@ -0,0 +1,72 @@
+package EjemploFila;
+
+public class People {
+	
+	private String name; 
+	private String identification; 
+	private int age; 
+	private String medicalHistory;
+	
+
+	public People() {
+		super();
+		this.name = "";
+		this.identification = "";
+		this.age = 0;
+		this.medicalHistory = "";
+	} 
+	
+	
+	public People(String nombre, String cedula, int edad, String historialMedico) {
+		super();
+		this.name = nombre;
+		this.identification = cedula;
+		this.age = edad;
+		this.medicalHistory = historialMedico;
+	}
+
+
+	public String getNombre() {
+		return name;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.name = nombre;
+	}
+
+
+	public String getCedula() {
+		return identification;
+	}
+
+
+	public void setCedula(String cedula) {
+		this.identification = cedula;
+	}
+
+
+	public int getEdad() {
+		return age;
+	}
+
+
+	public void setEdad(int edad) {
+		this.age = edad;
+	}
+
+
+	public String getHistorialMedico() {
+		return medicalHistory;
+	}
+
+
+	public void setHistorialMedico(String historialMedico) {
+		this.medicalHistory = historialMedico;
+	} 
+	
+	
+	
+	
+
+}
---
+++ src/EjemploFila/QueueSisben.java
@@ -0,0 +1,36 @@
+package EjemploFila;
+
+import Queue.IQueue;
+import Queue.Queue;
+
+public class QueueSisben {
+
+	/*make a instance of interface IQueue*/
+	private IQueue<People> persons;
+
+	
+	public QueueSisben() {
+		/*create the object Queue<Persona>*/
+		persons = new Queue<People>(); 
+		
+	} 
+	
+	public IQueue<People> getFila(){
+		return persons; 
+	}
+	
+	
+	public static void main(String[] args) {
+		/*use the methods of Queue<Persona> a journey of interface IQueue*/
+		
+		QueueSisben queue = new QueueSisben(); 
+		
+		System.out.println(queue.getFila().getSize());
+		queue.getFila().enQueue(new People("mateo", "12345", 19, ""));
+		System.out.println(queue.getFila().getSize());
+		
+		
+	}
+	
+	
+}
