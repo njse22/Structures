3afcfcf97cb3c160b3d5dbe9a99f8db47fafacad
camdnd <39162405+camdnd@users.noreply.github.com>
2019-03-06 15:25:10 -0500

    Add files via upload
    testeala que rico

--- src/TetsStructures/TestHash.java
+++ src/TetsStructures/TestHash.java
@@ -11,15 +11,90 @@ import Hash.NonexistentKeyException;
 class TestHash {
 
 	private Hash<Integer, String> hash; 
+	private Hash<Integer, String> hash1; 
+	private Hash<Integer, String> hash2; 
 	
 	private void stageOne() {
 		hash = new Hash<Integer, String>();
-
 		for (int i = 0; i < 10; i++) {
 			hash.put(i, "#"+i);
 		}
-		
 	}	
+	
+	
+	@Test
+	void testStageOne()
+	{
+		stageOne();
+		assertTrue(hash.getSize() == 10);
+		assertFalse(hash.isEmpty());
+	}
+	
+	
+	private void stageTwo()
+	{
+		hash1 = new Hash<Integer, String>();
+		for (int i = 0; i < 10; i++) {
+			hash1.put(i, "#"+i);
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			
+				try {
+					hash1.remove(i);
+				} catch (HashIsEmptyException | NonexistentKeyException e) {
+					
+					e.printStackTrace();
+				}
+		
+		}
+		
+	}
+	
+	@Test
+	void testStageTwo()
+	{
+		stageTwo();
+		assertTrue(hash1.getSize() == 0);
+		assertTrue(hash1.isEmpty());
+	}
+	
+	
+	private void stageThree()
+	{
+		hash2 = new Hash<Integer, String>();
+		
+		for (int i = 0; i < 10; i++) {
+			hash1.put(i, "#"+i);
+		}
+	}
+//	@Test
+//	void testOne() {
+//		stageOne();
+//		
+//		assertTrue(hash.getSize() == 0);
+//		assertTrue(hash.isEmpty());
+//	
+//		assertTrue(hash.getSize() == 10);
+//		assertFalse(hash.isEmpty());
+//		
+//		assertTrue(hash.get(4).equals("#4"));
+//		
+//		assertTrue(hash.get(8).equals("#8"));
+//		
+//		for (int i = 0; i < 10; i++) {
+//			try {
+//				hash.remove(i);
+//			}catch (Exception e) {
+//				fail(" No debería pasar "); 
+//			}
+//				
+//		}
+//		
+//		assertTrue(hash.getSize() == 0);
+//		assertTrue(hash.isEmpty());
+//		
+//	}
 	
 	private void stageEmty() {
 		hash = new Hash<Integer, String>();
@@ -33,67 +108,42 @@ class TestHash {
 		assertTrue(hash.isEmpty());
 	}
 	
-
-	@Test
-	void testOne() {
-		stageOne();
-		
-		assertTrue(hash.getSize() == 10);
-		assertFalse(hash.isEmpty());
-		
-		assertTrue(hash.get(4).equals("#4"));
-		
-		assertTrue(hash.get(8).equals("#8"));
-		
-		for (int i = 0; i < 10; i++) {
-				try {
-					hash.remove(i);
-				} catch (HashIsEmptyException | NonexistentKeyException e) {
-				
-					e.printStackTrace();
-				}
-				
-				
-		}
-		
-		assertTrue(hash.getSize() == 0);
-		assertTrue(hash.isEmpty());
-		
-	}
-
-	@Test
-	void testTwo() {
-		stageEmty();
-		
-		try {
-			hash.remove(0);
-		} catch (HashIsEmptyException | NonexistentKeyException e1) {
-			assertTrue(e1 instanceof HashIsEmptyException);
-		}
-		
-		for (int i = 0; i < 10; i++) {
-			hash.put(i, "#"+i);
-		}
 	
-		try {
-			hash.remove(12);
-		} catch (HashIsEmptyException | NonexistentKeyException e2) {
-			assertTrue(e2 instanceof NonexistentKeyException);
-		}
-		
-		for (int i = 0; i < 10; i++) {
-			try {
-				hash.remove(i);
-			}catch (Exception e) {
-				fail(" No debería pasar "); 
-			}
-				
-		}
-		
-		assertTrue(hash.getSize() == 0);
-		assertTrue(hash.isEmpty());
-		
-	}
+
+
+//	@Test
+//	void testTwo() {
+//		stageOne();
+//		
+//		try {
+//			hash.remove(0);
+//		} catch (HashIsEmptyException | NonexistentKeyException e1) {
+//			assertTrue(e1 instanceof HashIsEmptyException);
+//		}
+//		
+//		for (int i = 0; i < 10; i++) {
+//			hash.put(i, "#"+i);
+//		}
+//	
+//		try {
+//			hash.remove(12);
+//		} catch (HashIsEmptyException | NonexistentKeyException e2) {
+//			assertTrue(e2 instanceof NonexistentKeyException);
+//		}
+//		
+//		for (int i = 0; i < 10; i++) {
+//			try {
+//				hash.remove(i);
+//			}catch (Exception e) {
+//				fail(" No debería pasar "); 
+//			}
+//				
+//		}
+//		
+//		assertTrue(hash.getSize() == 0);
+//		assertTrue(hash.isEmpty());
+//		
+//	}
 
 	@Test
 	void testThree() {
@@ -118,6 +168,8 @@ class TestHash {
 				hash.put(i, "#"+i);
 				hash.put(i ,"#"+i+i); 
 				hash.remove(i);
+				
+				
 			} catch (HashIsEmptyException | NonexistentKeyException e) {}
 			
 		}
